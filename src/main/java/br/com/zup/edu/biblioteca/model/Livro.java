f09dfbf740b1c9c101ef282c0404b249a547fe6e
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-12 19:01:50 -0300

    Criando método para verificar se o livro é de uma determinada circulação

--- src/main/java/br/com/zup/edu/biblioteca/model/Livro.java
+++ src/main/java/br/com/zup/edu/biblioteca/model/Livro.java
@@ -25,17 +25,22 @@ public class Livro {
     @Column(nullable = false)
     private LocalDate dataPublicacao;
 
-    public Livro(String titulo, String descricao, String ISBN, LocalDate dataPublicacao) {
+    public Livro(String titulo, String descricao, String ISBN, LocalDate dataPublicacao, TipoCirculacao circulacao) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.ISBN = ISBN;
         this.dataPublicacao = dataPublicacao;
+        this.circulacao = circulacao;
     }
 
     /**
      * @deprecated construtor para uso exclusivo do Hibernate
      */
     public Livro() {
+    }
+
+    public Boolean eDoTipo(TipoCirculacao tipoCirculacao) {
+        return this.circulacao == tipoCirculacao;
     }
 
     public Long getId() {
